e06efd8e7ce3eda553bf95a60a1775127f42aead
GitExplorer88 <nanil6304@gmail.com>
2024-09-17 19:03:36 +0530

    model trainer

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from secondmlProject import logger
 from secondmlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from secondmlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from secondmlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from secondmlProject.pipeline.stage04_model_trainer import ModelTrainerTrainingPipeline
 
 
 STAGE_NAME= "Data Ingestion stage"
@@ -35,6 +36,17 @@ except Exception as e:
     logger.exception(e)
     raise e
 
+
+STAGE_NAME = "Model Trainer stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainerTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
 
 
